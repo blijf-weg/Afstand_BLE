6367a50af8cf95158bcfb5cdb7e1cbe32e381496
NathanSlembrouck <nathan.slembrouck@student.kuleuven.be>
2021-03-10 12:46:54 +0100

    met_mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
 #include <BLECast.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 #ifndef BEACON
 
@@ -11,12 +13,23 @@
 
     const int scanTimeSeconds = 1;
 
+    const char* mqtt_server = "192.168.1.2";
+
+    const char* ssid = "NETGEAR68";
+    const char* password = "excitedtuba713";
+
+    WiFiClient espClient;
+    PubSubClient client(espClient);
+    long lastMsg = 0;
+    char msg[50];
+    int value = 0;
+
 
     class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
     {
         void onResult(BLEAdvertisedDevice advertisedDevice)
         {
-            if (strcmp(advertisedDevice.getName().c_str(), "Radiation") >= 0)
+            if (strcmp(advertisedDevice.getName().c_str(), "RadiationNathan") == 0)
             {
                 Serial.print(advertisedDevice.getName().c_str());
                 Serial.printf(": %d \n", advertisedDevice.getRSSI());
@@ -25,10 +38,60 @@
         }
     };
 
+    void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8269Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/rssi/nathan");
+      client.subscribe("esp32/humidity");
+      client.subscribe("esp32/RSSI");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
     void setup()
     {
         Serial.begin(115200);
         Serial.println("Scanning...");
+
+        setup_wifi();
+        client.setServer(mqtt_server, 1883);
+        //client.setCallback(callback);
+        BLEDevice::init("Radiation SCAN");
+        pBLEScan = BLEDevice::getScan(); // create new scan
+        pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+        pBLEScan->setActiveScan(false); // active scan (true) uses more power, but get results faster
+        pBLEScan->setInterval(100);
+        pBLEScan->setWindow(99); // less or equal setInterval value
 
         BLEDevice::init("Radiation SCAN");
         pBLEScan = BLEDevice::getScan(); // create new scan
@@ -40,7 +103,23 @@
 
     void loop()
     {
-        BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
+        if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 1000) {
+    lastMsg = now;
+        
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(69, 1, 2, tempString);
+    //Serial.print("Temperature: ");
+    //Serial.println(tempString);
+    //client.publish("esp32/temperature", tempString);
+
+    BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
         pBLEScan->clearResults();
     }
 #else
