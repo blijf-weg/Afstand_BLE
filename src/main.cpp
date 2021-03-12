06fe1da1eeb1dd458246be3135ef173c16c6d019
NathanSlembrouck <nathan.slembrouck@student.kuleuven.be>
2021-03-12 23:52:50 +0100

    aangepast naar thuisnetwerk

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,21 @@
 #include <BLECast.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <iostream>
+//#include <cstring>
 
 #ifndef BEACON
+
+using namespace std;
 
     BLEScan *pBLEScan;
 
     const int scanTimeSeconds = 1;
 
-    const char* mqtt_server = "192.168.1.2";
+    const char* mqtt_server = "192.168.0.137";
 
-    const char* ssid = "NETGEAR68";
-    const char* password = "excitedtuba713";
+    const char* ssid = "telenet-648FE13";
+    const char* password = "YF74spyvpdkp";
 
     WiFiClient espClient;
     PubSubClient client(espClient);
@@ -31,9 +35,16 @@
         {
             if (strcmp(advertisedDevice.getName().c_str(), "RadiationNathan") == 0)
             {
-                Serial.print(advertisedDevice.getName().c_str());
-                Serial.printf(": %d \n", advertisedDevice.getRSSI());
-                Serial.printf(": %s \n", advertisedDevice.getManufacturerData().c_str());
+                //Serial.print(advertisedDevice.getName().c_str());
+                //Serial.printf(": %d \n", advertisedDevice.getRSSI());
+                //Serial.printf(": %s \n", advertisedDevice.getManufacturerData().c_str());
+                int rssi = advertisedDevice.getRSSI();
+                char cstr[16];
+                char* karakters = itoa(rssi, cstr, 10);
+                String rssistring= String(rssi) + "_1";
+                client.publish("esp32/rssi/nathan",rssistring.c_str());
+                //client.publish("esp32/rssi/nathan", karakters);
+                Serial.println(rssistring.c_str());
             }
         }
     };
@@ -62,10 +73,10 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8269Client")) {
+    if (client.connect("ESP8269Client1")) {
       Serial.println("connected");
       // Subscribe
-      client.subscribe("esp32/rssi/nathan");
+      //client.subscribe("esp32/rssi/nathan");
       client.subscribe("esp32/humidity");
       client.subscribe("esp32/RSSI");
     } else {
@@ -122,10 +133,11 @@ void reconnect() {
     BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
         pBLEScan->clearResults();
     }
+  }
 #else
     // define BTLE name
     // CAREFUL: each character eats into your usable adv packet length
-    BLECast bleCast("RadiationBeacon");
+    BLECast bleCast("RadiationNathan");
     
     uint8_t cnt = 0;
     char data[5];
@@ -140,20 +152,7 @@ void reconnect() {
 
     void loop()
     {
-        // note -- if you have too much data, it will not be added to the adv payload
-
-        if (cnt == 20){
-            // reset
-            cnt = 0;
-        }
-
-        if (cnt == 0){
-            // regenerate "random" data
-            int red = random(20, 50);
-            int orange = random(50, 70);
-            sprintf(data, "%02d&%02d", red, orange);
-        }
-        cnt += 1;
+        
         
         std::string s = bleCast.setManufacturerData(data, sizeof(data));
         Serial.println(s.c_str());
