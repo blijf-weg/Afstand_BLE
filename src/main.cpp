da7bf0e6dd864906b4a9e8d8a0453b27b7e6eabf
NathanSlembrouck <nathan.slembrouck@student.kuleuven.be>
2021-03-14 23:57:03 +0100

    toggle

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,33 @@
 #include <esp_bt.h>
 //#include <cstring>
 
-#ifndef BEACON
-
 using namespace std;
 
-    BLEScan *pBLEScan;
+#ifndef BEACON
 
-    const int scanTimeSeconds = 1;
+int counterReceiver = 0;
+int counterBeacon = 0;
+bool beacon = true;
 
-    const char* mqtt_server = "192.168.0.137";
+BLEScan *pBLEScan;
+BLECast bleCast("RadiationNathan");
 
-    const char* ssid = "telenet-648FE13";
-    const char* password = "YF74spyvpdkp";
+const int scanTimeSeconds = 1;
 
-    WiFiClient espClient;
-    PubSubClient client(espClient);
-    long lastMsg = 0;
-    char msg[50];
-    int value = 0;
+const char* mqtt_server = "192.168.0.137";
+
+const char* ssid = "telenet-648FE13";
+const char* password = "YF74spyvpdkp";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+uint8_t cnt = 0;
+char data[5];
+
 
 
     class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
@@ -38,7 +47,8 @@ using namespace std;
             {
                 //Serial.print(advertisedDevice.getName().c_str());
                 //Serial.printf(": %d \n", advertisedDevice.getRSSI());
-                //Serial.printf(": %s \n", advertisedDevice.getManufacturerData().c_str());
+                Serial.printf(":binnenkomendeData %s \n", advertisedDevice.getManufacturerData().c_str());
+                Serial.println((int)advertisedDevice.getManufacturerData().c_str());
                 int rssi = advertisedDevice.getRSSI();
                 char cstr[16];
                 char* karakters = itoa(rssi, cstr, 10);
@@ -46,27 +56,32 @@ using namespace std;
                 client.publish("esp32/rssi/nathan",rssistring.c_str());
                 //client.publish("esp32/rssi/nathan", karakters);
                 Serial.println(rssistring.c_str());
+                counterReceiver++;
+                if (counterReceiver == 3 && (int)advertisedDevice.getManufacturerData().c_str()!=1){
+                    counterReceiver = 0;
+                    beacon = true;
+                }
             }
         }
     };
 
-    void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
@@ -94,25 +109,23 @@ void reconnect() {
     {
         Serial.begin(115200);
         Serial.println("Scanning...");
-
         setup_wifi();
         client.setServer(mqtt_server, 1883);
-        //client.setCallback(callback);
-        BLEDevice::init("Radiation SCAN");
-        pBLEScan = BLEDevice::getScan(); // create new scan
-        pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
-        pBLEScan->setActiveScan(false); // active scan (true) uses more power, but get results faster
-        pBLEScan->setInterval(100);
-        pBLEScan->setWindow(99); // less or equal setInterval value
 
         BLEDevice::init("Radiation SCAN");
         pBLEScan = BLEDevice::getScan(); // create new scan
         pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
-        pBLEScan->setActiveScan(false); // active scan (true) uses more power, but get results faster
+        pBLEScan->setActiveScan(false); // active scan (true) uses more power, but get results faster, kheb dit zelf effe op true gezet
         pBLEScan->setInterval(100);
         pBLEScan->setWindow(99); // less or equal setInterval value
+
+        if (beacon){
+            bleCast.begin();
+        }
 
         esp_err_t esp_ble_tx_power_set(7);
+
+        sprintf(data,"%d",1);
     }
 
     void loop()
@@ -129,12 +142,21 @@ void reconnect() {
     // Convert the value to a char array
     char tempString[8];
     dtostrf(69, 1, 2, tempString);
-    //Serial.print("Temperature: ");
-    //Serial.println(tempString);
-    //client.publish("esp32/temperature", tempString);
-
-    BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
-        pBLEScan->clearResults();
+    if (!beacon){
+        BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
+            pBLEScan->clearResults();
+    }
+    if (beacon){
+        std::string s = bleCast.setManufacturerData(data, sizeof(data));
+        Serial.println(s.c_str());
+        delay(1000);
+        counterBeacon++;
+        if (counterBeacon == 3){
+            counterBeacon = 0;
+            beacon = false;
+            sprintf(data,"%d",8);
+        }
+    }
     }
   }
 #else
@@ -156,10 +178,10 @@ void reconnect() {
 
     void loop()
     {
-        
-        
+        sprintf(data,"%d",1);
         std::string s = bleCast.setManufacturerData(data, sizeof(data));
-        Serial.println(s.c_str());
+        //Serial.println(s.c_str());
+        Serial.println(data);
         delay(1000);
     }
 #endif
