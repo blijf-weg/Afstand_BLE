8d24d0b3221e17a51f5ad86588400c90f2eed369
DavidHemeryck <david.hemeryck@outlook.com>
2021-04-06 10:44:36 +0200

    Update main.cpp
    Snel scannen

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,29 @@
 #include <esp_bt.h>
 
 using namespace std;
-const uint_32_t = 0.1;
+
+volatile int interruptCounter;
+
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  Serial.println("Timer interrupt");
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+const uint32_t scanTimeSeconds = 0;
 //const int scanTimeSeconds = 1;
-const char* mqtt_server = "192.168.43.101"; //192.168.0.137
+const char* mqtt_server = "192.168.137.1";
+//"192.168.1.2"; 
+//"192.168.43.101";
+//192.168.0.137
 //const char* ssid = "telenet-648FE13";
 //const char* password = "YF74spyvpdkp";
-const char* ssid = "hot";
-const char* password = "hothothot";
+const char* ssid = "D84Z82H2 9418";
+const char* password = "73U-229k";
 const char* esp_naam = "Afstand_3";
 
 WiFiClient Afstand_3;
@@ -34,48 +50,49 @@ char data[5];
 
 bool send_to_broker = true;
 
-    class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+    void onResult(BLEAdvertisedDevice advertisedDevice)
     {
-        void onResult(BLEAdvertisedDevice advertisedDevice)
+        if (strcmp(advertisedDevice.getName().c_str(), "Afstand_0") == 0)
         {
-            if (strcmp(advertisedDevice.getName().c_str(), "Afstand_0") == 0)
-            {
-                int rssi = advertisedDevice.getRSSI();
-                String rssistring= String(rssi) + "_0";
-                if(send_to_broker){
-                    client.publish("esp32/afstand/rssi",rssistring.c_str());
-                    Serial.println(rssistring.c_str());
-                }
-            }
-            else if (strcmp(advertisedDevice.getName().c_str(), "Afstand_1") == 0)
-            {
-                int rssi = advertisedDevice.getRSSI();
-                String rssistring= String(rssi) + "_1";
-                if(send_to_broker){
-                    client.publish("esp32/afstand/rssi",rssistring.c_str());
-                    Serial.println(rssistring.c_str());
-                }
-            }
-            else if (strcmp(advertisedDevice.getName().c_str(), "Afstand_2") == 0)
-            {
-                int rssi = advertisedDevice.getRSSI();
-                String rssistring= String(rssi) + "_2";
-                if(send_to_broker){
-                    client.publish("esp32/afstand/rssi",rssistring.c_str());
-                    Serial.println(rssistring.c_str());
-                }
-            }
-            else if (strcmp(advertisedDevice.getName().c_str(), "Afstand_3") == 0)
-            {
-                int rssi = advertisedDevice.getRSSI();
-                String rssistring= String(rssi) + "_3";
-                if(send_to_broker){
-                    client.publish("esp32/afstand/rssi",rssistring.c_str());
-                    Serial.println(rssistring.c_str());
-                }
+            int rssi = advertisedDevice.getRSSI();
+            String rssistring= String(rssi) + "_0" + esp_naam[8];
+            if(send_to_broker){
+                client.publish("esp32/afstand/rssi",rssistring.c_str());
+                //Serial.println(rssistring.c_str());
             }
         }
-    };
+        else if (strcmp(advertisedDevice.getName().c_str(), "Afstand_1") == 0)
+        {
+            int rssi = advertisedDevice.getRSSI();
+            String rssistring= String(rssi) + "_1" + esp_naam[8];
+            if(send_to_broker){
+                client.publish("esp32/afstand/rssi",rssistring.c_str());
+                Serial.println(rssistring.c_str());
+            }
+        }
+        else if (strcmp(advertisedDevice.getName().c_str(), "Afstand_2") == 0)
+        {
+            int rssi = advertisedDevice.getRSSI();
+            String rssistring= String(rssi) + "_2" + esp_naam[8];
+            if(send_to_broker){
+                client.publish("esp32/afstand/rssi",rssistring.c_str());
+                Serial.println(rssistring.c_str());
+            }
+        }
+        else if (strcmp(advertisedDevice.getName().c_str(), "Afstand_3") == 0)
+        {
+            Serial.println(advertisedDevice.getRSSI());
+            int rssi = advertisedDevice.getRSSI();
+            String rssistring= String(rssi) + "_3" + esp_naam[8];
+            if(send_to_broker){
+                client.publish("esp32/afstand/rssi",rssistring.c_str());
+                Serial.println(rssistring.c_str());
+            }
+        }
+    }
+};
 
 void setup_wifi() {
     delay(10);
@@ -89,6 +106,7 @@ void setup_wifi() {
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
+
     }
     Serial.println("");
     Serial.println("WiFi connected");
@@ -115,7 +133,7 @@ void reconnect() {
   }
 }
 
-    void callback(char* topic, byte* message, unsigned int length) {
+void callback(char* topic, byte* message, unsigned int length) {
     Serial.print("Message arrived on topic: ");
     Serial.print(topic);
     Serial.print(". Message: ");
@@ -140,50 +158,64 @@ void reconnect() {
     }
 }
 
-    void setup()
-    {
-        pinMode(34, OUTPUT);
+void setup() {
 
-        Serial.begin(115200);
-        Serial.println("Scanning...");
-        setup_wifi();
-        client.setServer(mqtt_server, 1883);
-        client.setCallback(callback);
+    Serial.begin(115200);
 
-        BLEDevice::init("Radiation SCAN");
-        pBLEScan = BLEDevice::getScan(); // create new scan
-        pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
-        pBLEScan->setActiveScan(true); // active scan (true) uses more power, but get results faster, kheb dit zelf effe op true gezet
-        pBLEScan->setInterval(100);
-        pBLEScan->setWindow(99); // less or equal setInterval value
+    /*timer = timerBegin(0, 80000, true);
+    //1 tick = 1ms 
+    timerAttachInterrupt(timer, &onTimer, true);
+    timerAlarmWrite(timer, 60000, true);
+    timerAlarmEnable(timer);*/  
 
-        bleCast.begin();
+    Serial.println("Scanning...");
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
 
-        if (esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV,ESP_PWR_LVL_P9) == OK)
-            Serial.println("Transmission power changed\n");
+    BLEDevice::init("Radiation Scan");
+    pBLEScan = BLEDevice::getScan(); // create new scan
+    pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks(),true);
+    pBLEScan->setActiveScan(true); // active scan (true) uses more power, but get results faster, kheb dit zelf effe op true gezet
+    pBLEScan->setInterval(100);
+    pBLEScan->setWindow(99); // less or equal setInterval value
 
-        sprintf(data,"%d",9);
+    bleCast.begin();
+
+    if (esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV,ESP_PWR_LVL_P9) == OK) {
+        Serial.println("Transmission power changed\n");
     }
+    sprintf(data,"%d",9);
+}
 
-    void loop()
-    {
-        //ledcWriteTone(34, 1000);
+void loop() {
+    //zorgen dat de connectie met de broker actief blijft
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
 
-        if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+    long now = millis();
+    if (now - lastMsg > 1000) {
+        lastMsg = now;
+    }  
 
-  long now = millis();
-  if (now - lastMsg > 1000) {
-    lastMsg = now;
-  }    
-    // Convert the value to a char array
+    
+    /* Convert the value to a char array
     char tempString[8];
-    dtostrf(69, 1, 2, tempString);
-        BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
-            pBLEScan->clearResults();
-        std::string s = bleCast.setManufacturerData(data, sizeof(data));
-        Serial.println(s.c_str());
-        delay(1000);
+    dtostrf(69, 1, 2, tempString);*/
+
+
+    Serial.println("Scanning");
+    //BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
+    BLEScanResults foundDevices = pBLEScan->start(5);
+    pBLEScan->clearResults();
+    
+
+    if (interruptCounter > 0) {
+        portENTER_CRITICAL(&timerMux);
+        interruptCounter--;
+        portEXIT_CRITICAL(&timerMux);
+        Serial.print("An interrupt as occurred.");
+    }
 }
