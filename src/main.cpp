41dc2f49ac2461f2408b290fff24b83061133a9d
NathanSlembrouck <nathan.slembrouck@student.kuleuven.be>
2021-04-21 16:11:10 +0200

    bereken afstanden
    Toevoeging van de functie om alle onderlinge afstanden te berekenen op basis van de coördinaten.

--- src/main.cpp
+++ src/main.cpp
@@ -51,15 +51,18 @@ void IRAM_ATTR onTimer() {
 
 const uint32_t scanTimeSeconds = 0;
 //const int scanTimeSeconds = 1;
-const char* mqtt_server = "192.168.137.1";
+//const char* mqtt_server = "192.168.137.1";
+const char* mqtt_server = "192.168.43.101";
 //"192.168.1.2"; 
 //"192.168.43.101";
 //192.168.0.137
 //const char* ssid = "telenet-648FE13";
 //const char* password = "YF74spyvpdkp";
-const char* ssid = "D84Z82H2 9418";
-const char* password = "73U-229k";
-const char* esp_naam = "Afstand_4";
+//const char* ssid = "D84Z82H2 9418";
+//const char* password = "73U-229k";
+const char* ssid = "hot";
+const char* password = "hothothot";
+const char* esp_naam = "Afstand_4_Nathan";
 
 WiFiClient Afstand_4;
 PubSubClient client(Afstand_4);
@@ -124,8 +127,8 @@ class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
                 String rssistring = (String) buffer2.getAverage() + "_2" + esp_naam[8];
                 client.publish("esp32/afstand/rssi",rssistring.c_str());
                 client.publish("esp32/afstand/rssi",rssistring.c_str());
-                //Serial.print("Buffer2 gemiddelde: ");
-                //Serial.println(rssistring.c_str());
+                Serial.print("Buffer2 gemiddelde: ");
+                Serial.println(rssistring.c_str());
             }
         }
         /*else if (strcmp(advertisedDevice.getName().c_str(), "Afstand_3") == 0)
@@ -140,6 +143,53 @@ class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
         }*/
     }
 };
+
+bool bepaalCentraal(double* p1, double* p2, double* p3, double* p4, double limiet){
+    double x12 = abs(p1[0]-p2[0]);
+    double y12 = abs(p1[1]-p2[1]);
+    double l12 = sqrt(y12*y12+x12*x12);
+    cout << l12 << endl;
+    double x13 = abs(p3[0]-p1[0]);
+    double y13 = abs(p3[1]-p1[1]);
+    double l13 = sqrt(x13*x13+y13*y13);
+    cout << l13 << endl;
+    double x14 = abs(p4[0]-p1[0]);
+    double y14 = abs(p4[1] - p1[0]);
+    double l14 = sqrt(x14*x14 + y14*y14);
+    cout << l14 << endl;
+    return !(l12 < limiet || l13 < limiet || l14 < limiet);
+}
+
+bool bepaalTweede(double* p2, double* p3, double* p4, double limiet){
+    double x23 = abs(p2[0]-p3[0]);
+    double y23 = abs(p2[1]-p3[1]);
+    double l23 = sqrt(x23*x23 + y23*y23);
+    cout << l23 << endl;
+    double x24 = abs(p2[0]-p4[0]);
+    double y24 = abs(p2[1]-p4[1]);
+    double l24 = sqrt(x24*x24 + y24*y24);
+    cout << l24 << endl;
+    return !(l23 < limiet || l24 < limiet);
+
+}
+
+bool bepaalDerde(double* p3, double* p4, double limiet){
+    double x34 = abs(p3[0]-p4[0]);
+    double y34 = abs(p3[1]-p4[1]);
+    double l34 = sqrt(x34*x34 + y34*y34);
+    cout << l34 << endl;
+    return l34 > limiet;
+}
+
+bool checkAfstanden(double* p1, double* p2, double* p3, double* p4, double limiet){
+    bool result1 = bepaalCentraal(p1, p2, p3, p4, limiet);
+    cout << result1 << endl;
+    bool result2 = bepaalTweede(p2, p3, p4, limiet);
+    cout << result2 << endl;
+    bool result3 = bepaalDerde(p3, p4, limiet);
+    cout << result3 << endl;
+    return (result1 && result2 && result3);
+}
 
 void setup_wifi() {
     delay(10);
@@ -219,6 +269,7 @@ void setup() {
     timerAlarmEnable(timer);*/  
 
     Serial.println("Scanning...");
+    Serial.println("allej werk");
     setup_wifi();
     client.setServer(mqtt_server, 1883);
     client.setCallback(callback);
